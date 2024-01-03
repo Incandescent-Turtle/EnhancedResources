28d73acda72418fd15421d262616e4b31270098d
Incandescent-Turtle <rorsmacd@gmail.com>
2024-01-03 12:24:25 -0800

    changed to version 0.0.1

--- src/main/java/mod/RLander/enhancedresources/util/Reference.java
+++ src/main/java/mod/RLander/enhancedresources/util/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
 		public static final String MOD_ID = "enhancedresources";
 		public static final String NAME = "Enhanced Resources";
-		public static final String VERSION = "1.0";
+		public static final String VERSION = "0.0.1";
 		public static final String ACCEPTED_VERSIONS = "[1.12.2]";
 		public static final String CLIENT_PROXY_CLASS = "mod.RLander.enhancedresources.proxy.ClientProxy";
 		public static final String COMMON_PROXY_CLASS = "mod.RLander.enhancedresources.proxy.CommonProxy";	
